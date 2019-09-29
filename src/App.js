d9d9ca946f70aaa7526380d6e5522c09d80688b5
BarKot <kotniewiczbartosz@gmai.com>
2019-09-29 18:29:44 +0200

    add new commponent Treebars

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './scss/App.scss';
 import MoviesContainer from "./app/movies/components/MoviesContainer";
 import MoviesForm from "./app/movies/components/MoviesForm";
 import MoviesReset from "./app/movies/components/MoviesRemove";
-import Nav from './app/Components/Nav/nav'
+import Nav from './app/Components/nav'
+import ThreeMusketeers from "./app/Components/ThreeMusketeers";
 
 function App() {
     return (
         <div className="App">
         <Nav/>
+        <ThreeMusketeers/>
         </div>
     );
 }
---
+++ src/app/Components/ThreeMusketeers.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import ReactSVG from "react-svg";
+
+class ThreeMusketeers extends Component {
+    render() {
+        return <div className='container'>
+            <div className='info'>
+                <div className='info__bar'>
+                    <span>10</span>
+                    <em>ODDANYCH WORKÓW</em>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam est eum laboriosam optio quibusdam totam!</p>
+                </div>
+                <div className='info__bar'>
+                    <span>10</span>
+                    <em>ODDANYCH WORKÓW</em>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam est eum laboriosam optio quibusdam totam!</p>
+                </div>
+                <div className='info__bar'>
+                    <span>10</span>
+                    <em>ODDANYCH WORKÓW</em>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam est eum laboriosam optio quibusdam totam!</p>
+                </div>
+            </div>
+        </div>
+
+    }
+}
+
+
+export default ThreeMusketeers
