79b2a667addeaedccca8dc45fc2f434aa78e78d0
BarKot <kotniewiczbartosz@gmai.com>
2019-10-03 16:27:45 +0200

    break

--- src/app/Components/Home/ContactForm.js
+++ src/app/Components/Home/ContactForm.js
@@ -43,98 +43,103 @@ const Write = () => {
             saveFn(d);
         }
     }
-        async function saveFn(newmassage) {
 
-            try {
-                const response = await fetch(url, {
-                    method: 'POST',
-                    body: JSON.stringify(newmassage),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
-                const json = await response.json();
-                console.log('Success:', JSON.stringify(json));
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        }
+    async function saveFn(newmassage) {
 
-        const fetchTextForm = e => {
-            e.preventDefault();
-
-
-            let newmassage = {
-                name: nameInput.current.value,
-                email: emailInput.current.value,
-                message: messageInput.current.value
-            };
-
-            const rules = {
-                'Rname': /^[a-zA-Z ]{2,30}$/,
-                'Remail': /^[a-z0-9\._%-]+@[a-z0-9\.-]+\.[a-z]{2,4}$/i,
-                'Rmessage': /^[a-zA-Z ]{5,100}$/
-
-            };
-
-            async function checkAll() {
-                const breakpoints = {
-                    a: '',
-                    b: '',
-                    c: ''
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(newmassage),
+                headers: {
+                    'Content-Type': 'application/json'
                 }
-                const resultsA = await checkForm(rules.Rname, newmassage.name, setName);
-                breakpoints.a = resultsA;
-                const resultsB = await checkForm(rules.Remail, newmassage.email, setEmail);
-                breakpoints.b = resultsB;
-                const resultsC = await checkForm(rules.Rmessage, newmassage.message, setMessage);
-                breakpoints.c = resultsC;
+            });
+            const json = await response.json();
+            console.log('Success:', JSON.stringify(json));
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
 
-                checkSend(breakpoints.a, breakpoints.b, breakpoints.c, newmassage);
+    const fetchTextForm = e => {
+        e.preventDefault();
+
+
+        let newmassage = {
+            name: nameInput.current.value,
+            email: emailInput.current.value,
+            message: messageInput.current.value
+        };
+
+        const rules = {
+            'Rname': /^[a-zA-Z ]{2,30}$/,
+            'Remail': /^[a-z0-9\._%-]+@[a-z0-9\.-]+\.[a-z]{2,4}$/i,
+            'Rmessage': /^[a-zA-Z ]{5,100}$/
+
+        };
+
+        async function checkAll() {
+            const breakpoints = {
+                a: '',
+                b: '',
+                c: ''
             }
+            const resultsA = await checkForm(rules.Rname, newmassage.name, setName);
+            breakpoints.a = resultsA;
+            const resultsB = await checkForm(rules.Remail, newmassage.email, setEmail);
+            breakpoints.b = resultsB;
+            const resultsC = await checkForm(rules.Rmessage, newmassage.message, setMessage);
+            breakpoints.c = resultsC;
 
-            checkAll();
+            checkSend(breakpoints.a, breakpoints.b, breakpoints.c, newmassage);
         }
 
+        checkAll();
+    }
+
+    let wrongName = '';
+
+    if (name === false) {
+         wrongName = <span>Podane imie jest nieprawidłowe</span>;
+    }
 
 
+    return <div className='contact__form'>
+        <p>Skontaktuj sie z nami</p>
+        <ReactSVG src={DecorationSVG}/>
 
-
-return <div className='contact__form'>
-    <p>Skontaktuj sie z nami</p>
-    <ReactSVG src={DecorationSVG}/>
-
-    <form onSubmit={fetchTextForm}>
-        <table>
-            <tbody>
-            <tr className='table__name'>
-                <th className='table__name--padding'>
-                    <span>Wpisz swoje imie</span><br/>
-                    <input ref={nameInput} className='table__name--style' type='text'/><br/>
-                    <hr/>
-                </th>
-                <th>
-                    <span>Wpisz swój email</span><br/>
-                    <input ref={emailInput} className='table__name--style' type='text'/><br/>
-                    <hr/>
-                </th>
-            </tr>
-            <tr className='table__area'>
-                <th>
-                    <span>Wpisz swoją wiadomośc</span><br/>
-                    <textarea ref={messageInput} className='table__area--style'/><br/>
-                    <hr/>
-                </th>
-            </tr>
-            <tr>
-                <th className='table__button'>
-                    <button className='table__button--style'>Wyślij</button>
-                </th>
-            </tr>
-            </tbody>
-        </table>
-    </form>
-</div>
+        <form onSubmit={fetchTextForm}>
+            <table>
+                <tbody>
+                <tr className='table__name'>
+                    <th className='table__name--padding'>
+                        <span>Wpisz swoje imie</span><br/>
+                        <input ref={nameInput} className='table__name--style' type='text'/><br/>
+                        <hr/>
+                        {wrongName}
+                    </th>
+                    <th>
+                        <span>Wpisz swój email</span><br/>
+                        <input ref={emailInput} className='table__name--style' type='text'/><br/>
+                        <hr/>
+                    </th>
+                </tr>
+                <tr className='table__area'>
+                    <th>
+                        <span>Wpisz swoją wiadomośc</span><br/>
+                        <textarea ref={messageInput} className='table__area--style'/><br/>
+                        <hr/>
+                    </th>
+                </tr>
+                <tr>
+                    <th className='table__button'>
+                        <button className='table__button--style'>Wyślij</button>
+                    </th>
+                </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
 
 };
 
